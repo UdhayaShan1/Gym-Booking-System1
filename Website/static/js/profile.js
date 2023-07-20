486c7c6999a834d8eb7ab69b89cea4afcfa672d8
Udhaya <udhaya.shan1@gmail.com>
2023-07-20 16:24:35 +0800

    0.6.8 Website - CSS, organisation

--- Website/static/js/profile.js
+++ Website/static/js/profile.js
@@ -5,6 +5,56 @@ document.addEventListener("DOMContentLoaded", function() {
     var spotterNameCell = document.getElementById("spotterName");
     var spotterRoomCell = document.getElementById("spotterRoom");
 
+    var reportButton = document.getElementById("reports");
+    reportButton.addEventListener("click", function(e) {
+        e.preventDefault();
+        window.location.href = "/report";
+    })
+
+    var equipmentButton = document.getElementById("equipment")
+    equipmentButton.addEventListener("click", function(e) {
+        e.preventDefault();
+        window.location.href = "/equipmentpage"
+    })
+
+    var editBookingButton = document.getElementById("editBookingButton");
+    editBookingButton.addEventListener("click", function(e) {
+        e.preventDefault();
+        window.location.href = "/edit_bookings";
+    });
+
+    var profileButton = document.getElementById("profileButton");
+    profileButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        window.location.href = "/profile";
+    });
+
+
+
+
+    let logoutButton = document.getElementById("logoutButton");
+    logoutButton.addEventListener("click", function(event){
+        event.preventDefault();
+        fetch("/logout", {
+            method: "POST"
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === "success") {
+                window.alert("You have logged out");
+                window.location.href = "/";
+            }
+            else {
+                window.alert("Error logging out");
+            }
+        })
+        .catch(error => {
+            console.error("Error", error);
+            window.alert("Error logging out");
+        })
+    });
+
+
     fetch("/user_details")
         .then(response => response.json())
         .then(data => {
@@ -248,6 +298,49 @@ document.addEventListener("DOMContentLoaded", function() {
             window.location.href = "/change_pwd";
         })
     })
+
+
+
+    //Nav-Bar Buttons
+    var bookingButton = document.getElementById("bookingButton");
+    bookingButton.addEventListener("click", function(event) {
+        fetch("/profile_completed")
+        .then(response => response.json())
+        .then(data => 
+            {
+                if (data.status === "success") {
+                    window.location.href = "/booking_page";
+                }
+                else {
+                    window.alert(data.message)
+                    window.location.href = "/profile"
+                }
+            })
+        .catch(error => {console.error("Error", error);
+        window.alert("Error, try again");
+        })
+    });
+
+
+
+    var changePassword = document.getElementById("changePassword");
+    changePassword.addEventListener("click", function(event) {
+        event.preventDefault();
+        fetch("/send_otp_changepwd")
+        .then(response => response.json())
+        .then(data => {
+            window.alert("OTP sent to your email!")
+            window.location.href = "/change_pwd";
+        })
+    })
+
+
+
+
+
+
+
+
 
 
 });
