5b0051fcd4fc10a9b3c8b5f19d7f37894d444e56
Udhaya <udhaya.shan1@gmail.com>
2023-08-08 23:21:20 +0800

    0.7.1 Website - Equipment feedback system.

--- Website/databaseconn.py
+++ Website/databaseconn.py
@@ -9,3 +9,14 @@ db = mysql.connector.connect(
     database="fitbook",
     connect_timeout=30
 )
+
+def dbcreator():
+    pwd = "pscale_pw_6FwzujGzhViG30ZzrXTr8HpVFaOz33j2SK5w4LHo9VH"
+    db = mysql.connector.connect(
+        host="gcp.connect.psdb.cloud",
+        user='2m2rl8qq29djzzqvooet',
+        passwd=pwd,
+        database="fitbook",
+        connect_timeout=30
+    )
+    return db
